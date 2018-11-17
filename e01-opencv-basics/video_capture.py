204c80ce9e1d9887de5d8f60987b91129cd8ca47
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2018-11-17 15:46:22 +0900

    +задания в тетрадке, +про показ ч/б изображений

--- e01-opencv-basics/video_capture.py
+++ e01-opencv-basics/video_capture.py
@@ -1,23 +1,23 @@
-	import cv2
-	cap = cv2.VideoCapture(0)
+import cv2
+cap = cv2.VideoCapture(0)
 
-	while(True):
-	    # Получить очередной кадр
-	    ret, frame = cap.read()
+while(True):
+    # Получить очередной кадр
+    ret, frame = cap.read()
 
-	    # Преобразовать в ч/б
-	    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
+    # Преобразовать в ч/б
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
 
-	    # Нарисовать подсказку о выходе
-	    cv2.putText(gray, "Press `q` to quit", (20,20), cv2.FONT_HERSHEY_SIMPLEX, 1, 0)
+    # Нарисовать подсказку о выходе
+    cv2.putText(gray, "Press `q` to quit", (20,20), cv2.FONT_HERSHEY_SIMPLEX, 1, 0)
 
-	    # Показать
-	    cv2.imshow('Video Capture Test',gray)
+    # Показать
+    cv2.imshow('Video Capture Test',gray)
 
-	    # Если нажата q, выйти, иначе начать снова
-	    if cv2.waitKey(1) & 0xFF == ord('q'):
-	        break
+    # Если нажата q, выйти, иначе начать снова
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-	# Закрыть камеру и окна 
-	cap.release()
-	cv2.destroyAllWindows()
+# Закрыть камеру и окна 
+cap.release()
+cv2.destroyAllWindows()
